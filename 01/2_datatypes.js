fc9c5efa92a814c344e716ebabf4c6b3888c979d
Ankit <dasankit2485@gmail.com>
2025-05-11 14:59:45 +0000

    datatype

---
+++ 01/2_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  //it will treat the js code as new version
+
+// alert(3+3) it will not print the value but if we use console of a browser it will print 
+
+/* datatypes
+number => 2 power 53
+bigint => for bing number basically use in trading where big numbers are used
+string => "" , ''
+boolean => true / false
+null => standalone value
+undefined => value is not define  like   let city; it will show undefined value on printing it 
+symbol => spcially use for showing difference between components uniqueness
+objects 
+*/
+
+console.log(typeof "ankit")    //it is used to find the type of the variables or values
+console.log(typeof null);     //its type is object 
+console.log(typeof undefined);//its type is undefined
